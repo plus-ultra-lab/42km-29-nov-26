bb4490df272ee02255a9fe63005a0fc8cd5339fb
mattpoad <100686009+mattpoad@users.noreply.github.com>
2026-08-23 19:03:00 +0200

    Update 23 aug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='plan-marathon-40cc14dc';
+const C='plan-marathon-4c68724c';
 const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==C).map(n=>caches.delete(n)))).then(()=>self.clients.claim()))});
